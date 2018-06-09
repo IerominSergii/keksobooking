b7e5d2d498f0fb1b9bf4356b8d58c239c11fb0e2
IerominSergii <iconboooook@gmail.com>
2018-06-09 13:18:27 +0300

    Исправь загрузку фото жилья

--- js/map.js
+++ js/map.js
@@ -1,24 +1,8 @@
 'use strict';
 
 var AD_POSTS_AMOUNT = 4;
-var getRandomNumber = function (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-};
 
-var isIncluded = function (array, element) {
-  var isAlreadyIncluded = false;
-  if (array.length > 0) {
-    isAlreadyIncluded = array.some(function (it) {
-      return it === element;
-    });
-
-    return isAlreadyIncluded;
-  }
-
-  return isAlreadyIncluded;
-};
-
-// game-data
+// === game-data ===
 var offer = {
   title: [
     'Большая уютная квартира',
@@ -48,9 +32,29 @@ var offerType = {
   palace: 'Дворец',
 };
 
-var getRandomAdPost = function (advertPostsAlreadyDone) {
+// === functions ===
+var getRandomNumber = function (min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+var isIncluded = function (array, element) {
+  var isAlreadyIncluded = false;
+  if (array.length > 0) {
+    isAlreadyIncluded = array.some(function (it) {
+      return it === element;
+    });
+
+    return isAlreadyIncluded;
+  }
+
+  return isAlreadyIncluded;
+};
+
+var getRandomAdvertPost = function (advertPostsAlreadyDone) {
   var adPost = {};
+  // author
   adPost.author = {};
+  // author.avatar
   var authorAlreadyDone = advertPostsAlreadyDone.map(function (it) {
     return it.author.avatar;
   });
@@ -59,7 +63,9 @@ var getRandomAdPost = function (advertPostsAlreadyDone) {
     adPost.author.avatar = 'img/avatars/user0' + getRandomNumber(1, 8) + '.png';
   } while (isIncluded(authorAlreadyDone, adPost.author.avatar));
 
+  // offer
   adPost.offer = {};
+  // offer.title
   var titlesAlreadyDone = advertPostsAlreadyDone.map(function (it) {
     return it.offer.title;
   });
@@ -67,15 +73,21 @@ var getRandomAdPost = function (advertPostsAlreadyDone) {
   do {
     adPost.offer.title = offer.title[getRandomNumber(0, offer.title.length - 1)];
   } while (isIncluded(titlesAlreadyDone, adPost.offer.title));
-
+  // offer.address
   adPost.offer.address = getRandomNumber(100, 999) + ', ' + getRandomNumber(100, 999);
+  // offer.price
   adPost.offer.price = getRandomNumber(1000, 1000000);
+  // offer.type
   adPost.offer.type = offer.type[getRandomNumber(0, offer.type.length - 1)];
+  // offer.rooms
   adPost.offer.rooms = getRandomNumber(1, 5);
-  // в задании указано случайное количество гостей, беру от 1 до 10
+  // offer.guests в задании указано случайное количество гостей, беру от 1 до 10
   adPost.offer.guests = getRandomNumber(1, 10);
+  // offer.checkin
   adPost.offer.checkin = offer.checkin[getRandomNumber(0, offer.checkin.length - 1)];
+  // offer.checkout
   adPost.offer.checkout = offer.checkout[getRandomNumber(0, offer.checkout.length - 1)];
+  // offer.features
   var featuresLength = getRandomNumber(1, 6);
 
   adPost.offer.features = [];
@@ -85,8 +97,9 @@ var getRandomAdPost = function (advertPostsAlreadyDone) {
     var featureElement = newFeaturesList.splice(randomPosition, 1);
     adPost.offer.features.push(featureElement);
   }
-
+  // offer.description
   adPost.offer.description = '';
+  // offer.photos
   adPost.offer.photos = [];
   var newPhotosList = offer.photos.slice();
   for (var j = 0; j < offer.photos.length; j++) {
@@ -94,29 +107,27 @@ var getRandomAdPost = function (advertPostsAlreadyDone) {
     var photo = newPhotosList.splice(randomPhotoPosition, 1);
     adPost.offer.photos.push(photo[0]);
   }
-
+  // location
   adPost.location = {};
+  // location.x
   adPost.location.x = getRandomNumber(300, 900);
+  // location.y
   adPost.location.y = getRandomNumber(130, 630);
 
   return adPost;
 };
 
-var generateAdPosts = function (amount) {
+var generateAdvertPosts = function (amount) {
   var adPostsList = [];
   for (var k = 1; k <= amount; k++) {
-    adPostsList.push(getRandomAdPost(adPostsList));
+    adPostsList.push(getRandomAdvertPost(adPostsList));
   }
 
   return adPostsList;
 };
 
-var map = document.querySelector('.map');
-var mapPin = document.querySelector('template').content.querySelector('.map__pin');
-var mapPinsContainer = document.querySelector('.map__pins');
-
-var renderPin = function (advert) {
-  var pin = mapPin.cloneNode(true);
+var renderPin = function (advert, pinTemplate) {
+  var pin = pinTemplate.cloneNode(true);
   pin.style.left = advert.location.x + 'px';
   pin.style.top = advert.location.y + 'px';
   pin.querySelector('img').src = advert.author.avatar;
@@ -125,23 +136,8 @@ var renderPin = function (advert) {
   return pin;
 };
 
-// у блока .map убери класс .map--faded
-map.classList.remove('map--faded');
-
-var advertPosts = generateAdPosts(AD_POSTS_AMOUNT);
-var fragment = document.createDocumentFragment();
-
-advertPosts.forEach(function (advert) {
-  fragment.appendChild(renderPin(advert));
-});
-
-mapPinsContainer.appendChild(fragment);
-
-
-var mapCard = document.querySelector('template').content.querySelector('.map__card');
-
-var renderCard = function (post) {
-  var card = mapCard.cloneNode(true);
+var renderCard = function (post, cardTemplate) {
+  var card = cardTemplate.cloneNode(true);
   card.querySelector('.popup__avatar').src = post.author.avatar;
   card.querySelector('.popup__title').textContent = post.offer.title;
   card.querySelector('.popup__text--address').textContent = post.offer.address;
@@ -169,18 +165,36 @@ var renderCard = function (post) {
   card.querySelector('.popup__description').textContent = post.offer.description;
 
   var photosContainer = card.querySelector('.popup__photos');
-  var photoImage = photosContainer.querySelector('img');
-  photoImage.src = post.offer.photos[0];
+  var photoImageTemplate = photosContainer.removeChild(photosContainer.querySelector('img'));
 
-  for (var p = 1; p < post.offer.photos.length; p++) {
-    var nextPhoto = photoImage.cloneNode(true);
-    nextPhoto.src = post.offer.photos[p];
+  post.offer.photos.forEach(function (it, index) {
+    var nextPhoto = photoImageTemplate.cloneNode(true);
+    nextPhoto.src = post.offer.photos[index];
     photosContainer.appendChild(nextPhoto);
-  }
+  });
 
   return card;
 };
 
-var titleCard = renderCard(advertPosts[0]);
+// === elements ===
+var map = document.querySelector('.map');
+var mapPin = document.querySelector('template').content.querySelector('.map__pin');
+var mapPinsContainer = document.querySelector('.map__pins');
+var mapCard = document.querySelector('template').content.querySelector('.map__card');
 var mapFiltersContainer = map.querySelector('.map__filters-container');
+var fragment = document.createDocumentFragment();
+
+// === start ===
+// у блока .map убери класс .map--faded
+map.classList.remove('map--faded');
+// сгенерируй рекламные посты, количество - AD_POSTS_AMOUNT
+var advertPosts = generateAdvertPosts(AD_POSTS_AMOUNT);
+
+advertPosts.forEach(function (advert) {
+  fragment.appendChild(renderPin(advert, mapPin));
+});
+
+mapPinsContainer.appendChild(fragment);
+
+var titleCard = renderCard(advertPosts[0], mapCard);
 map.insertBefore(titleCard, mapFiltersContainer);
